45a0d0b796e0fb1f9d5ada4592c2daed1f08975e
alexsiqingliu <uilxela7@gmail.com>
2020-04-11 14:42:11 +0800

    create routes, delete duplicate mern file

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI;
+const users = require('./routes/api/users');
+const tweets = require('./routes/api/tweets');
 
 mongoose
   .connect(db, { useNewUrlParser: true})
@@ -10,7 +12,12 @@ mongoose
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get("/", (req, res) => res.send("Hello World"));
+app.get("/", (req, res) => {
+  console.log(req);
+  res.send("Hello World")
+});
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
+app.use('/api/users', users);
+app.use('/api/tweets', tweets);
