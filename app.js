8f3879c79c8134c6db8f3fa5ad4a48daad3b3fe9
alexsiqingliu <uilxela7@gmail.com>
2020-04-12 10:10:11 +0800

    add salt to password

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI;
 const users = require('./routes/api/users');
 const tweets = require('./routes/api/tweets');
+const User = require('./models/User');
+const bodyParser = require('body-parser');
 
 mongoose
   .connect(db, { useNewUrlParser: true})
@@ -12,7 +14,19 @@ mongoose
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+app.use(bodyParser.json());
+
 app.get("/", (req, res) => {
+  const user = new User({
+    handle: 'jim',
+    email: 'jim@jim.com',
+    password: 'jimisgreat123'
+  })
+  user.save();
   console.log(req);
   res.send("Hello World")
 });
