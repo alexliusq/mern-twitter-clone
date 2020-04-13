46324e7bfd795fc39471f9b8667f9f02bc477321
alexsiqingliu <uilxela7@gmail.com>
2020-04-13 14:54:49 +0800

    delete default files, create session_action

---
+++ frontend/src/actions/session_actions.js
@@ -0,0 +1,22 @@
+// src/actions/session_actions.js
+
+// impport whole thing, will add functions
+import * as APIUtil from '../util/session_api_util';
+import jwt_decode from 'jwt-decode';
+
+// This pattern should be familiar from full stack project
+
+export const RECEIVE_USER_LOGOUT = "RECEIVE_USER_LOGOUT";
+
+export const logoutUser = () => ({
+  type: RECEIVE_USER_LOGOUT
+});
+
+export const logout = () => dispatch => {
+  // Remove the token from local storage
+  localStorage.removeItem('jwtToken');
+  // Remove the token from the common axios header
+  APIUtil.setAuthToken(false);
+  // Dispatch logout action
+  dispatch(logoutUser());
+}
