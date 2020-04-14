51960a2a9a89042c29b50ba11a80d53136b02859
alexsiqingliu <uilxela7@gmail.com>
2020-04-14 08:53:59 +0800

    finish login/signup up reducers and actions

--- frontend/src/actions/session_actions.js
+++ frontend/src/actions/session_actions.js
@@ -6,7 +6,47 @@ import jwt_decode from 'jwt-decode';
 
 // This pattern should be familiar from full stack project
 
+export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
+export const RECEIVE_SESSION_ERRORS = "RECEIVE_SESSION_ERRORS";
 export const RECEIVE_USER_LOGOUT = "RECEIVE_USER_LOGOUT";
+export const RECEIVE_USER_SIGN_IN = "RECEIVE_USER_SIGN_IN";
+
+// dispatch when user signs in
+export const receiveCurrentUSer = currentUser => ({
+  type: RECEIVE_CURRENT_USER,
+  currentUser,
+});
+
+// Used to redirect user to login page on signup
+export const receiveUserSignIn = () => ({
+  type: RECEIVE_USER_SIGN_IN
+});
+
+// dispatch when there are authentication errors on frontend
+export const logoutUser = () => ({
+  type: RECEIVE_USER_LOGOUT
+});
+
+export const signup = user => dispatch => (
+  APIUtil.signup(user).then(() => (
+    dispatch(receiveUserSignIn())
+  ), err => (
+    dispatch(receiveErrors(err.response.data))
+  ))
+);
+
+export const login = user => dispatch => (
+  APIUtil.login(user).then(res => {
+    const { token } = res.data;
+    localStorage.setItem('jwtToken', token);
+    APIUtil.setAuthToken(token);
+    const decoded = jwt_decode(token);
+    dispatch(receiveCurrentUSer(decoded));
+  })
+  .catch(err => {
+    dispatch(receiveErrors(err.response.data));
+  })
+)
 
 export const logoutUser = () => ({
   type: RECEIVE_USER_LOGOUT
