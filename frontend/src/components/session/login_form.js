f642dc5319a3a57a55c410464ff90ced23942e91
alexsiqingliu <uilxela7@gmail.com>
2020-04-14 10:12:14 +0800

    finish login/signup forms

---
+++ frontend/src/components/session/login_form.js
@@ -0,0 +1,81 @@
+// src/components/session/login_form.js
+
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+
+class LoginForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: '',
+      errors: {}
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.renderErrors = this.renderErrors.bind(this);
+  }
+
+  //If User authenticated, redirect to the Tweets page
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.currentUser === true) {
+      this.props.history('/tweets');
+    }
+
+    // set or clear errors
+    this.setState({ errors: nextProps.errors });
+  }
+
+  // Handle form submission
+  handleSubmit(e) {
+    e.preventDefault();
+
+    let user = {
+      email: this.state.email,
+      password: this.state.password
+    };
+
+    this.props.login(user);
+  }
+
+  // Render session errors if any
+  renderErrors() {
+    return(
+      <ul>
+        {Object.keys(this.state.errors).map((error, i) => (
+          <li key={`error-${i}`}>
+            {this.state.errors[error]}
+          </li>
+        ))}
+      </ul>
+    )
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <input type="text"
+              value={this.state.password}
+              onChange={this.update('password')}
+              placeholder="Password"
+            />
+            <br/>
+            <input type="password"
+              value={this.state.password}
+              onChange={this.update('password')}
+              placeholder="Password"
+            />
+            <br/>
+            <input type="submit" value="Submit"/>
+            {this.renderErrors()}
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default withRouter(LoginForm);
