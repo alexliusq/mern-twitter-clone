8bc19a1fdf2b74d78b8d342e1465ce2c5d28fd06
alexsiqingliu <uilxela7@gmail.com>
2020-04-15 16:11:56 +0800

    configure for heroku deployment

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const tweets = require('./routes/api/tweets');
 const User = require('./models/User');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require('path');
 
 require('./config/passport')(passport);
 
@@ -16,6 +17,13 @@ mongoose
   .connect(db, { useNewUrlParser: true})
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
 
 app.use(passport.initialize());
 
